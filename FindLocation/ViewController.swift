4a0f2c42d7fd0351911465cb35e016d87b4bc275
Amit Sharma <amit.sharma2@soprasteria.com>
2018-11-12 11:12:38 +0530

    First commit

---
+++ FindLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindLocation
+//
+//  Created by SopraSteria on 12/11/18.
+//  Copyright © 2018 SopraSteria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
